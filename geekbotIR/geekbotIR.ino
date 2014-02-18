e3e4bb04f25964f491713f719f971f8bff7c10c6
Kyle Granat <kyle@trossenrobotics.com>
2014-02-18 08:05:44 -0600

    Panning servo now scans and effects the movement of the rover

--- geekbotIR/geekbotIR.ino
+++ geekbotIR/geekbotIR.ino
@@ -30,33 +30,86 @@
 //85 deg - 55 deg full cw
 
 //
-//Try Full 115 ccw / 45 cw
+//Try Full 115 ccw / 65 cw
 
 
 #include <Servo.h>
 
-#define CW_SPEED 45
-#define CCW_SPEED 115
+#define CW_SPEED 72 //increase if the rover is drifting left
+#define CCW_SPEED 113 //decrease if the rover is drifting right
   
-#define OBJECT_IR_READING 500
+#define OBJECT_IR_READING 400
   
 Servo servoLeft;
 Servo servoRight;
+Servo servoPan;
+
+
+long previousMillis = 0;        // will store last time LED was updated
+long interval = 2;           // interval at which to blink (milliseconds)
+
+int sign = 5; //positive when going up, negative when going down
+int panValue = 0;
 
 void setup()
 {
 
   servoLeft.attach(9); 
   servoRight.attach(10);
+  servoPan.attach(11);
   
   roverForward();
 }
 
 void loop()
 {
+  
+  
+  
+  
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis > interval) 
+  {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
+    
+    panValue = sign + panValue;
+    
+    if(panValue > 160)
+    {
+      sign = -2;
+    }
+    else if (panValue <20)
+    {
+      sign = 2;
+    }
+    
+    servoPan.write(panValue);
+  }
+  
+  
   if(analogRead(0) >OBJECT_IR_READING)
   {
-    roverStop();
+   //roverHeading(panValue);
+   
+   if(panValue > 110)
+   {
+    
+     roverRight(250); 
+   }
+   
+   else if(panValue < 70)
+   {
+     
+     roverLeft(250);
+   }
+   
+   else
+   {
+     roverBackward(1000);
+     roverLeft(500);
+   }
     
   }
   else
@@ -75,25 +128,61 @@ void roverForward()
   servoRight.write(CW_SPEED);
 }
 
-void roverBackward()
+void roverBackward(int time)
 {
   servoLeft.write(CW_SPEED);
   servoRight.write(CCW_SPEED);
+  delay(time);
 }
 
-void roverLeft()
+void roverLeft(int time)
 {
   servoLeft.write(CW_SPEED);
   servoRight.write(CW_SPEED);
+  delay(time);
 }
 
-void roverRight()
+void roverRight(int time)
 {
   servoLeft.write(CCW_SPEED);
   servoRight.write(CCW_SPEED);
+  delay(time);
 }
 void roverStop()
 {
   servoLeft.write(90);
   servoRight.write(90);
 }
+
+//heading is a value from 0 to 180. 90 is dead ahead, 0 is left, 180 is right
+void roverHeading(int heading)
+{
+  int centeredHeading = heading - 90; //center the heading so -90 is left, 0 is dead ahdaed and 90 is right
+  int turningServo;
+  
+  //if heading is positive, move t the right
+  if(centeredHeading > 0)
+  {
+    turningServo = map(centeredHeading, 0, 90, 95, CCW_SPEED); 
+    servoLeft.write(90);
+    servoRight.write(CW_SPEED);
+    
+  }
+  
+  else if(centeredHeading<0)
+  {
+    turningServo = map(centeredHeading, -90, 0, CW_SPEED, 85) ;
+    servoLeft.write(CCW_SPEED);
+    servoRight.write(90);
+    
+  }
+  
+  else
+  {
+    roverForward() ;
+  }
+  
+  
+  
+}
+
