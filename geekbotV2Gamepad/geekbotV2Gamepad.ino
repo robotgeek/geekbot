a81213f1b0d504f3109b9bc98ac43f2bdf526700
Renee <renee1382@gmail.com>
2016-09-22 12:28:31 -0500

    Feature packed update
    Fixed inability to decrease forward driving turning radius via
    controller.
    Increased controller timeout duration.
    Removed unnecessary delays().
    
    Remote control speed selection (high, med, low) using TB and A buttons
    (Top and Bottom).
    
    Remote control turning radius using B and TA buttons (Left and Right).
    
    Remote control trim adjustment using Select and Start buttons.

--- geekbotV2Gamepad/geekbotV2Gamepad.ino
+++ geekbotV2Gamepad/geekbotV2Gamepad.ino
@@ -33,7 +33,8 @@
 #include <Servo.h>     //include servo library to control continous turn servos
 #include "Sounds.h"
 #include "Gamepad.h"
-unsigned long last_gamepad_command = millis();
+unsigned long last_gamepad_command = millis(); //milliseconds since last command was received
+const unsigned long gamepad_timeout = 250; //milliseconds to wait without control input for stopping robot (150 minimum)
 
 //pin constants
 const int TRIM_KNOB_PIN = 0;
@@ -46,25 +47,40 @@ const int RIGHT_SERVO_PIN = 11;
 //Servo control
 const int SERVO_STOP = 1500; //servo pulse in microseconds for stopped servo
 
-const int CW_MIN_SPEED = 1400;
-const int CW_MAX_SPEED = 1250;
-
+//Speed constants from RGS-4C No Load Test Data
 const int CCW_MIN_SPEED = 1580;
+const int CW_MIN_SPEED = 1400;
 const int CCW_MAX_SPEED = 1750;
+const int CW_MAX_SPEED = 1250;
+//Experimental speed constant
+const int CCW_MED_SPEED = 1660;
+const int CW_MED_SPEED = 1300;
 
 int SERVO_DRIVE_TURN_SPEED = 70; //For turning while driving (controller selectable)
-const int SERVO_TURN_SPEED = 50; //For in place turning
+int SERVO_TURN_SPEED = 50; //For in place turning. Applied to CW and CCW_MIN_SPEEDs
+const int SERVO_TURN_SPEED_HIGH = 50;
+const int SERVO_TURN_SPEED_LOW = 25;
 
 Servo servoLeft, servoRight;      //wheel servo objects
+
 int servoSpeedLeft = SERVO_STOP;   //left servo speed.
 int servoSpeedRight = SERVO_STOP;  //right servo speed.
 
+//Wheel speeds from "gear" selection
 int leftFwdSpeed = CCW_MAX_SPEED;
 int leftRevSpeed = CW_MAX_SPEED;
 
 int rightFwdSpeed = CW_MAX_SPEED;
 int rightRevSpeed = CCW_MAX_SPEED;
 
+enum SpeedSelections
+{
+  SPEED_MIN,
+  SPEED_MED,
+  SPEED_MAX
+};
+
+int currentSpeed = SPEED_MAX;
 
 //Trim testing
 int _wheel_speed_trim = 0;
@@ -73,6 +89,8 @@ void updateDriveTrim()
   int knob_value = analogRead( TRIM_KNOB_PIN );
   _wheel_speed_trim = map( knob_value, 0, 1023, -100, 100 );
 }
+
+int _wheel_speed_trim_override = 0;
 
 void setup()
 {
@@ -108,8 +126,8 @@ void loop()
     if ( my_gamepad.button_press_up() )
     {
       Serial.print( "UP" );
-      servoSpeedLeft = leftFwdSpeed + _wheel_speed_trim;
-      servoSpeedRight = rightFwdSpeed + _wheel_speed_trim;
+      servoSpeedLeft = leftFwdSpeed + _wheel_speed_trim + _wheel_speed_trim_override;
+      servoSpeedRight = rightFwdSpeed + _wheel_speed_trim + _wheel_speed_trim_override;
       if ( my_gamepad.button_press_left() )
       {
         Serial.print( " and LEFT" );
@@ -153,16 +171,22 @@ void loop()
     if ( my_gamepad.button_press_start() )
     {
       Serial.print( "START" );
+      SoundPlay(UP);
+      _wheel_speed_trim_override += 5;
+      my_gamepad.update_button_states();
     }
     if ( my_gamepad.button_press_select() )
     {
       Serial.print( "SELECT" );
+      SoundPlay(DOWN);
+      _wheel_speed_trim_override -= 5;
+      my_gamepad.update_button_states();
     }
 
     if ( my_gamepad.button_press_b() )
     {
-      Serial.print( "B" );
-      if ( SERVO_DRIVE_TURN_SPEED < 10 )
+      Serial.print( "B" ); //(Left button)
+      if ( SERVO_DRIVE_TURN_SPEED > 10 )
       {
         SERVO_DRIVE_TURN_SPEED -= 10;
         SoundPlay(DOWN);
@@ -177,15 +201,61 @@ void loop()
     }
     if ( my_gamepad.button_press_tb() )
     {
-      Serial.print( "TB" );
+      Serial.print( "TB" ); //(Top button)
+      switch( currentSpeed )
+      {
+      case SPEED_MIN:
+        SoundPlay(UP);
+        SERVO_TURN_SPEED = SERVO_TURN_SPEED_HIGH;
+        currentSpeed = SPEED_MED;
+        leftFwdSpeed = CCW_MED_SPEED;
+        leftRevSpeed = CW_MED_SPEED;
+        rightFwdSpeed = CW_MED_SPEED;
+        rightRevSpeed = CCW_MED_SPEED;
+        break;
+      case SPEED_MED:
+        SoundPlay(UP);
+        currentSpeed = SPEED_MAX;
+        leftFwdSpeed = CCW_MAX_SPEED;
+        leftRevSpeed = CW_MAX_SPEED;
+        rightFwdSpeed = CW_MAX_SPEED;
+        rightRevSpeed = CCW_MAX_SPEED;
+        break;
+      default:
+        SoundPlay(UHOH);
+      }
+      my_gamepad.update_button_states();
     }
     if ( my_gamepad.button_press_a() )
     {
-      Serial.print( "A" );
+      Serial.print( "A" ); //(Bottom button)
+      switch( currentSpeed )
+      {
+      case SPEED_MED:
+        SoundPlay(DOWN);
+        currentSpeed = SPEED_MIN;
+        SERVO_TURN_SPEED = SERVO_TURN_SPEED_LOW;
+        leftFwdSpeed = CCW_MIN_SPEED + 20;
+        leftRevSpeed = CW_MIN_SPEED - 20;
+        rightFwdSpeed = CW_MIN_SPEED - 20;
+        rightRevSpeed = CCW_MIN_SPEED + 20;
+        break;
+      case SPEED_MAX:
+        SoundPlay(DOWN);
+        currentSpeed = SPEED_MED;
+        leftFwdSpeed = CCW_MED_SPEED;
+        leftRevSpeed = CW_MED_SPEED;
+        rightFwdSpeed = CW_MED_SPEED;
+        rightRevSpeed = CCW_MED_SPEED;
+        break;
+      default:
+        SoundPlay(UHOH);
+      }
+      my_gamepad.update_button_states();
     }
     if ( my_gamepad.button_press_ta() )
     {
-      Serial.print( "TA" );
+      Serial.print( "TA" ); //(Right button)
       if ( SERVO_DRIVE_TURN_SPEED < 200 )
       {
         SERVO_DRIVE_TURN_SPEED += 10;
@@ -195,8 +265,6 @@ void loop()
       {
         SoundPlay(UHOH);
       }
-
-      delay(250);
       my_gamepad.update_button_states();
     }
 
@@ -207,7 +275,7 @@ void loop()
     Serial.print( " R: " );
     Serial.println( servoSpeedRight );
   }
-  else if ( last_gamepad_command + 150 < millis() )
+  else if ( last_gamepad_command + gamepad_timeout < millis() )
   {
     servoSpeedLeft = SERVO_STOP;
     servoSpeedRight = SERVO_STOP;
